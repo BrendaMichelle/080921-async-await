a3aa8c4b2f2b2957a7eae87cc04e9fedf2a43412
Michelle <brendamichelle995@gmail.com>
2021-08-26 11:29:30 -0400

    before lecture

---
+++ index.js
@@ -0,0 +1,11 @@
+// async await 
+
+function getFoxPic() {
+  fetch("https://randomfox.ca/floof/")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  console.log(data)
+}
+
+getFoxPic()
