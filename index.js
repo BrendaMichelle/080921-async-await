d6f310f9391e78865b69ca8bfe5f045bdee695f2
Michelle <brendamichelle995@gmail.com>
2021-08-26 11:51:28 -0400

    after async await

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 // async await 
 
-function getFoxPic() {
-  fetch("https://randomfox.ca/floof/")
-    .then(response => response.json())
-    .then(data => console.log(data))
 
-  console.log(data)
+// getFoxPic()
+
+
+// async function getFoxPic() {
+//   fetch("https://randomfox.ca/floof/")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
+
+
+// function getFoxPic() {
+//   fetch("https://randomfox.ca/floof/")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
+
+// function renderFoxPic(data) {
+//   console.log(data)
+// }
+
+const getFoxPic = async () => {
+  const response = await fetch("https://randomfox.ca/floof/")
+  const data = await response.json()
+  return data
 }
 
-getFoxPic()
+// console.log(getFoxPic())
+
+getFoxPic().then(data => console.log(data))
+
+// const response = await fetch("https://randomfox.ca/floof/") // WILL NOT WORK
